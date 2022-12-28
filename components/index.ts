ccce6b6735dca9315cd1fb7adbeed932d9d1153f
ryann254 <keddelyronjoz@gmail.com>
2022-12-28 11:07:58 +0300

    feat: add Discover UI

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,7 @@
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
+import Discover from './Discover';
+import Footer from './Footer';
+import SuggestedAccounts from './SuggestedAccounts';
 
-export { Navbar, Sidebar };
+export { Navbar, Sidebar, Discover, SuggestedAccounts, Footer };
